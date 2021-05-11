ce05b3f32a66dd4ea407f0a6ae6bdb17e65b3438
JoKing <joebryant614@gmail.com>
2021-05-11 13:12:12 -0700

    half way there

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,11 +1,10 @@
-
 const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
 
 jest.mock('../lib/Potion');
 
 test('create a player object', () => {
-    const player = new Player('Shaft')
+    const player = new Player('Shaft');
 
     expect(player.name).toBe('Shaft')
     expect(player.health).toEqual(expect.any(Number));
@@ -17,4 +16,23 @@ test('create a player object', () => {
         expect.arrayContaining([expect.any(Object)])
     );
     
+});
+
+test('gets player stats as an object', () => {
+    const player = new Player('Shaft');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns flase', () => {
+    const player = new Player('Shaft');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
 });
